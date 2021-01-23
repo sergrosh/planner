6c2ce7494f44cf7b0f9a3e11242484cd0334b49e
Sergii Roshchupkin <s.roshchupkin@carfax.eu>
2021-01-23 21:37:04 +0100

    Add family rest controller

---
+++ src/main/java/com/familyplanner/planner/family/FamilyController.java
@@ -0,0 +1,43 @@
+package com.familyplanner.planner.family;
+
+import java.util.Collection;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/family", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(value = "Family controller", tags = "family")
+public class FamilyController {
+
+    private FamilyRepository familyRepository;
+
+    public FamilyController(FamilyRepository familyRepository) {
+        this.familyRepository = familyRepository;
+    }
+
+    /**
+     * Get family {@link Family} by surname.
+     *
+     * @param surname surname
+     * @return family
+     */
+    @CrossOrigin
+    @ApiOperation(value = "Get families by surname", tags = "Get families")
+    @GetMapping(value = "/{surname}")
+    public ResponseEntity<Collection<Family>> getFamily(@PathVariable("surname") String surname) {
+        Collection<Family> families = familyRepository.findByLastName(surname);
+        if (families.isEmpty()) {
+            return ResponseEntity.badRequest().body(null);
+        }
+        return ResponseEntity.ok(families);
+    }
+
+}
